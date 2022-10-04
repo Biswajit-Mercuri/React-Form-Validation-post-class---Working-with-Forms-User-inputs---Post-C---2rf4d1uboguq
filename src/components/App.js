6588c321adfd57813532a6cea4f12f84adb6e1c9
mercuri <mercuri90028@gmail.com>
2022-10-04 08:10:06 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,44 @@ import React, { useState, useRef } from 'react';
 
 function App() {
 
- /**
-  * code here
-  */
+  /**
+   * code here
+   */
+  const fnameRef = useRef();
+  const emailRef = useRef();
+  const [error, setError] = useState(undefined);
+  const [data, setData] = useState({ fname: undefined, lname: undefined });
 
-  return(
+  const change = () => {
+    // console.log(emailRef.current.value);
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
+      setError(undefined);
+      document.getElementById("submit").disabled = false;
+    } else {
+      setError("Email is invalid");
+      document.getElementById("submit").disabled = true;
+    }
+  };
+
+
+  return (
     <div className="App">
       <h1>How About Them Apples</h1>
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setData({
+          fname: fnameRef.current.value,
+          lname: emailRef.current.value
+        });
+      }}>
         <fieldset>
           <label>
             <p>First Name</p>
-            <input id='fname' name="name"  ref={fnameRef}/>
+            <input id='fname' name="name" ref={fnameRef} />
             <br></br>
             <p>Email</p>
-            <input id='lname' name="name"   ref={emailRef}/>
-            {error && <h2 style={{color: 'red'}}>{error}</h2>}
+            <input id='lname' name="name" onChange={change} ref={emailRef} />
+            {error && <h2 style={{ color: 'red' }}>{error}</h2>}
           </label>
         </fieldset>
 
@@ -33,8 +55,8 @@ function App() {
       {
         data.fname != undefined && (
           <div>
-          <h1>{data.fname}</h1>
-          <h2>{data.lname}</h2>
+            <h1>{data.fname}</h1>
+            <h2>{data.lname}</h2>
           </div>
         )
       }
